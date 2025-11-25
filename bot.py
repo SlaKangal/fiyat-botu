035cf9b4f224b23a34191a07eab37f21acbd0d63
SlaKangal <sla.kangal0@gmail.com>
2025-11-26 02:42:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from email.mime.text import MIMEText
 import sys
 
 # --- AYARLAR ---
-URL = "https://www.trendyol.com/apple/iphone-13-128gb-yildiz-isigi-p-150244342"
+URL = "https://www.trendyol.com/apple/iphone-13-128-gb-yildiz-isigi-cep-telefonu-apple-turkiye-garantili-p-150059024"
 HEDEF_FIYAT = 40000 
 
 # --- BİLGİLERİN ---
@@ -84,3 +84,4 @@ def fiyat_kontrol_et():
 
 if __name__ == "__main__":
     fiyat_kontrol_et()
+
